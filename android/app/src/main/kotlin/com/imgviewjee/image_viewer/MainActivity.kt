57374423838701690082b09689e758e91366ae7c
Jeevanandhan06 <106806022+Jeevanandhan06@users.noreply.github.com>
2023-06-27 22:22:24 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/imgviewjee/image_viewer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.imgviewjee.image_viewer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(  MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Image viewer'),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/shirt.jpg'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
